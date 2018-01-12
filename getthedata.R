1196f39c1b8ad2b499b37da0738a9130bdc2fb6c
Yulia Zamriy <yulia.zamriy@gmail.com>
2018-01-12 15:53:00 -0500

    input data and initial code

---
+++ getthedata.R
@@ -0,0 +1,27 @@
+
+# install.packages("RSocrata")
+# library("RSocrata")
+# allSitesDataFrame <- ls.socrata("https://soda.demo.socrata.com")
+# nrow(allSitesDataFrame) # Number of datasets
+# allSitesDataFrame$title # Names of each dataset
+#Schedule <- read_csv("DDC_sampledata Schedule.csv", col_types = ctypes)
+
+setwd("C:/Users/yzamriy/Documents/Tools and Methodology/DS/Git/DDS")
+library(tidyverse)
+
+Projects <- read_csv("DDC_sampledata Projects.csv")
+
+ctypes <- paste("cc", strrep("D", 49), sep = '')
+Schedule <- read_csv("DDC_sampledata Schedule.csv")
+Budget <- read_csv("DDC_sampledata Budget.csv")
+
+str(Projects)
+str(Schedule)
+str(Budget)
+
+x <- as.Date(Schedule$ProjectedProjectStart, format = '%Y-%m-%d')
+
+datefor <- function(x) as.Date(x, format = '%Y-%m-%d')
+Schedule[,3:51] <- lapply(Schedule[,3:51], datefor)
+
+table(Schedule$ProjectedProjectStart)
